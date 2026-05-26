e71e05ab2b336618f641f6527c05062a94fa6fd1
Arthur Neves <82537546+ArthurNeves0512@users.noreply.github.com>
2026-05-26 01:25:44 -0300

    feature(interfaces): acessing the constant_pool to get the value instead showing only index

--- src/lib/class_loader/fields_interfaces.h
+++ src/lib/class_loader/fields_interfaces.h
@@ -9,11 +9,14 @@ void readInterfaces(ClassFile *class_file_ptr, FILE *file_ptr) {
     printf("interfaces_count: %hu\n", class_file_ptr->interfaces_count);
 
     class_file_ptr->interfaces = (u2*) malloc(class_file_ptr->interfaces_count * sizeof(u2));
-
     for (u2 i = 0; i < class_file_ptr->interfaces_count; i++) {
         u2 index = u2Read(file_ptr);
         class_file_ptr->interfaces[i] = index;
-        printf("interface[%hu]: #%hu\n", i, index);
+        CONSTANT_Class_info * class_info = class_file_ptr->constant_pool[index].constant_class_info;
+        CONSTANT_Utf8_info * utf8_info = class_file_ptr->constant_pool[class_info->name_index].utf8_info;
+        printf("interface[%hu]:\n", i);
+        printf("\tindex at constant_pool: #%hu\n",index);
+        printf("\tvalue: %s\n",utf8_info->bytes);
     }
 }
 
