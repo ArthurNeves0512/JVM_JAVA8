2d13a124307ee5d2663dc8805d2717731f57d9db
Arthur <arthur.henrique529@gmail.com>
2026-06-11 21:19:50 -0300

    adicionando testes dos opcodes

---
+++ data/examples/SwitchTest.java
@@ -0,0 +1,21 @@
+public class SwitchTest {
+
+    public static void main(String[] args) {
+
+        int x = 2;
+
+        switch (x) {
+
+            case 1:
+                System.out.println("um");
+                break;
+
+            case 2:
+                System.out.println("dois");
+                break;
+
+            default:
+                System.out.println("outro");
+        }
+    }
+}
---
+++ data/examples/TableSw.java
@@ -0,0 +1,33 @@
+public class TableSw {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        switch (x) {
+
+            case 1:
+                System.out.println("um");
+                break;
+
+            case 2:
+                System.out.println("dois");
+                break;
+
+            case 3:
+                System.out.println("tres");
+                break;
+
+            case 4:
+                System.out.println("quatro");
+                break;
+
+            case 5:
+                System.out.println("cinco");
+                break;
+
+            default:
+                System.out.println("outro");
+        }
+    }
+}
